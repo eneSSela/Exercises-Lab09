16998953d00c464e4b5c7fb0475bbed99d2aa341
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-19 22:11:23 +0100

    Implement Constructor + getCurrentFile

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,31 @@
 package it.unibo.mvc;
 
+import java.io.File;
 
 /**
  * Application controller. Performs the I/O.
  */
-public class Controller {
+public final class Controller {
+
+    private File currentFile;
+
+    /**
+     * Builds the new Controller and sets the default file
+     * to "output.txt" inside the user's home directory.
+     */
+    public Controller() {
+        final String home = System.getProperty("user.home");
+        final String separator = System.getProperty("file.separator");
+        this.currentFile = new File(home + separator + "output.txt");
+    }
+
+    /**
+     * Returns the currently selected file.
+     * 
+     * @return the current file
+     */
+    public File getCurrentFile() {
+        return this.currentFile;
+    }
 
 }
