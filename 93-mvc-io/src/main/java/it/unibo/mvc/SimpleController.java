d37b661f9eca1f236eb0074a818f2c92a7b9ab75
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-20 10:37:13 +0100

    Implement Constructor + setNextString of SimpleController

--- 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
+++ 93-mvc-io/src/main/java/it/unibo/mvc/SimpleController.java
@@ -1,9 +1,28 @@
 package it.unibo.mvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * 
- *
+ * A simple implementation of the Controller interface.
  */
 public final class SimpleController implements Controller {
 
+    private String currentString = "";
+    private final List<String> history;
+
+    /**
+     * Builds a Controller with an empty history
+     */
+    public SimpleController() {
+        this.history = new ArrayList<>();
+    }
+
+    @Override
+    public void setNextString(String s) {
+        if (s == null) {
+            throw new IllegalStateException("String cannot be null");
+        }
+        this.currentString = s;
+    }
 }
