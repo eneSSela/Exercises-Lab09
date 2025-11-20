111e93b0dc1b78451a6b7d162632fcf5bfb91aa1
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-20 09:30:06 +0100

    Implement the SimpleGUI Constructor

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,15 @@
 package it.unibo.mvc;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +17,41 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("Simple GUI");
 
+    /**
+     * Builds the whole graphical interface
+     */
+    public SimpleGUI() {
+        final JPanel panelText = new JPanel();
+        final JPanel panelSave = new JPanel();
+
+        frame.setLayout(new BorderLayout());
+        frame.add(panelText, BorderLayout.CENTER);
+        frame.add(panelSave, BorderLayout.SOUTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        final JTextArea text = new JTextArea();
+        panelText.add(text, BorderLayout.CENTER);
+
+        final JButton save = new JButton("Save");
+        panelSave.add(save, BorderLayout.CENTER);
+
+        final Controller controller = new Controller();
+
+        save.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    controller.save(text.getText());
+                } catch (final IOException ex) {
+                    JOptionPane.showMessageDialog(frame,
+                        "Error while selecting file:\n" + ex.getMessage(),
+                        "Error",
+                        JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            }
+        });
+    }
 }
