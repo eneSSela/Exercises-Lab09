d4c671ce9043bb7f33b511b5c4e9b3e1c8c591e3
Enes-Sela <enes.sela@studio.unibo.it>
2025-11-20 09:31:45 +0100

    Implement the Display Method

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -7,6 +7,8 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -17,6 +19,7 @@ import java.io.IOException;
  */
 public final class SimpleGUI {
 
+    private static final int PROPORTION = 5;
     private final JFrame frame = new JFrame("Simple GUI");
 
     /**
@@ -53,5 +56,34 @@ public final class SimpleGUI {
                 }
             }
         });
+    }
+
+    /**
+     * Displays the main frame, resizing it proportionally to the screen
+     */
+    private void display() {
+        /*
+        * Make the frame one fifth the resolution of the screen. This very method is
+        * enough for a single screen setup. In case of multiple monitors, the
+        * primary is selected. In order to deal coherently with multimonitor
+        * setups, other facilities exist (see the Java documentation about this
+        * issue). It is MUCH better than manually specify the size of a window
+        * in pixel: it takes into account the current resolution.
+        */
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / PROPORTION, sh / PROPORTION);
+        /*
+        * Instead of appearing at (0,0), upper left corner of the screen, this
+        * flag makes the OS window manager take care of the default positioning
+        * on screen. Results may vary, but it is generally the best choice.
+        */
+        frame.setLocationByPlatform(true);
+        frame.pack();
+        /*
+        * OK, ready to push the frame onscreen
+        */
+        frame.setVisible(true);
     }
 }
